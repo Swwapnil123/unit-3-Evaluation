48c8e53a75633985c0a15a195dd8eda1d9ff154d
Swwapnil123 <101570993+Swwapnil123@users.noreply.github.com>
2022-10-06 10:30:19 +0530

    Add files via upload

---
+++ JS 201-Evaluation-4/component/navbar.js
@@ -0,0 +1,10 @@
+let sideBar=()=>{
+    return `
+    <img src="" alt="" id="user_image">
+    <h3 id="user_name"> </h3>
+    <p id="user_email"></p>
+    <p id="user_country"></p>
+    `
+}
+
+export{sideBar}
